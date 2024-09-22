50781dc5ebac90a54c9e915e8f30a9c6983e6d88
halghasra <husain.alghasra@gmail.com>
2024-09-22 14:02:30 +0100

    Convert gatsby-ssr.js to use ES6 module syntax

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -10,7 +10,7 @@ import { ThemeProvider } from './src/components/ThemeContext';
 /**
  * @type {import('gatsby').GatsbySSR['onRenderBody']}
  */
-exports.onRenderBody = ({ setHtmlAttributes }) => {
+export const onRenderBody = ({ setHtmlAttributes }) => {
   setHtmlAttributes({ lang: `en` })
 }
 
