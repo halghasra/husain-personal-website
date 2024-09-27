7ac0daa1f593e455cd3679a77f683edd23969f7d
halghasra <husain.alghasra@gmail.com>
2024-09-28 00:27:21 +0100

    Missing the new Comments component

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { Disqus } from 'gatsby-plugin-disqus';
 
 const Comments = ({ post, siteUrl }) => {
+    if (!process.env.GATSBY_DISQUS_SHORTNAME) {
+        console.warn('Disqus shortname is not set. Comments will not be rendered.');
+        return <p>Comments are currently unavailable.</p>;
+      }
   const disqusConfig = {
     url: `${siteUrl}${post.fields.slug}`,
     identifier: post.id,
